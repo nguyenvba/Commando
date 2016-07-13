ac8433fa353545acb8334ae27b3de94014dc8226
DinhHieu <hieu96ptit@gmail.com>
2016-07-13 23:27:09 +0700

    Resize images

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var Commando = {};
+
+window.onload = function(){
+  Commando.game = new Phaser.Game(window.innerWidth,
+                                  window.innerHeight,
+                                  Phaser.AUTO,
+                                  '',
+                                  {preload:preload, create:create, update:update}
+                                  );
+}
+
+var preload = function(){
+  
+}
+
+var create = function(){
+
+}
+
+var update = function(){
+
+}
